ae42da6fdbc22d6be5f301bedc9e180f79fb5129
IKende <henryfan@msn.com>
2019-05-15 15:35:45 +0800

    add performance sample

---
+++ Samples/Performance/ClientApp/Program.cs
@@ -0,0 +1,142 @@
+﻿using BeetleX;
+using BeetleX.XRPC.Clients;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static XRPCClient client;
+        static IUserService UserService;
+        static int mCount;
+        static int[] mUsers = new int[] { 20, 50, 100, 200, 500 };
+        static void Main(string[] args)
+        {
+            client = new XRPCClient("192.168.2.18", 9090, 3);
+            client.Connect();
+            client.NetError = (c, e) =>
+            {
+                Console.WriteLine(e.Error.Message);
+            };
+            client.TimeOut = 10000;
+            UserService = client.Create<IUserService>();
+            Test();
+            Console.Read();
+        }
+
+        static async void Test()
+        {
+            foreach (int i in mUsers)
+                await Login(i);
+            foreach (int i in mUsers)
+                await Add(i);
+            foreach (int i in mUsers)
+                await List(i);
+
+        }
+
+        static async Task Login(int users)
+        {
+            mCount = 0;
+            List<Task> tasks = new List<Task>();
+            double start = TimeWatch.GetElapsedMilliseconds();
+            for (int i = 0; i < users; i++)
+            {
+                var item = Task.Run(async () =>
+                {
+                    for (int k = 0; k < 10000; k++)
+                    {
+                        var result = await UserService.Login("admin", "123456");
+                        System.Threading.Interlocked.Increment(ref mCount);
+                    }
+                });
+                tasks.Add(item);
+            }
+            Task.WaitAll(tasks.ToArray());
+            double useTime = TimeWatch.GetElapsedMilliseconds() - start;
+            Console.WriteLine($"[{DateTime.Now:t}][Login][Users:{users}|Count:{mCount}|Times:{useTime:######.000}ms|RPS:{(mCount / useTime * 1000):#######}]");
+            await Task.CompletedTask;
+        }
+
+        static async Task Add(int users)
+        {
+            mCount = 0;
+            List<Task> tasks = new List<Task>();
+            double start = TimeWatch.GetElapsedMilliseconds();
+            for (int i = 0; i < users; i++)
+            {
+                var item = Task.Run(async () =>
+                {
+                    for (int k = 0; k < 10000; k++)
+                    {
+                        var result = await UserService.Add("henry", "henryfan@msn.com", "guangzhou", "http://github.com");
+                        System.Threading.Interlocked.Increment(ref mCount);
+                    }
+                });
+                tasks.Add(item);
+            }
+            Task.WaitAll(tasks.ToArray());
+            double useTime = TimeWatch.GetElapsedMilliseconds() - start;
+            Console.WriteLine($"[{DateTime.Now:t}][Add][Users:{users}|Count:{mCount}|Times:{useTime:######.000}ms|RPS:{(mCount / useTime * 1000):#######}]");
+            await Task.CompletedTask;
+        }
+
+        static async Task List(int users)
+        {
+            mCount = 0;
+            List<Task> tasks = new List<Task>();
+            double start = TimeWatch.GetElapsedMilliseconds();
+            for (int i = 0; i < users; i++)
+            {
+                var item = Task.Run(async () =>
+                {
+                    for (int k = 0; k < 10000; k++)
+                    {
+                        var result = await UserService.List(5);
+                        System.Threading.Interlocked.Increment(ref mCount);
+                    }
+                });
+                tasks.Add(item);
+            }
+            Task.WaitAll(tasks.ToArray());
+            double useTime = TimeWatch.GetElapsedMilliseconds() - start;
+            Console.WriteLine($"[{DateTime.Now:t}][Add][Users:{users}|Count:{mCount}|Times:{useTime:######.000}ms|RPS:{(mCount / useTime * 1000):#######}]");
+            await Task.CompletedTask;
+        }
+    }
+
+    public interface IUserService
+    {
+        Task<bool> Login(string name, string pwd);
+
+        Task<User> Add(string name, string email, string city, string remark);
+
+        Task Save();
+
+        Task<User> Modify(User user);
+
+        Task<List<User>> List(int count);
+    }
+
+    [MessagePackObject]
+    public class User
+    {
+        [Key(4)]
+        public string ID { get; set; }
+
+        [Key(0)]
+        public string Name { get; set; }
+
+        [Key(1)]
+        public string City { get; set; }
+
+        [Key(2)]
+        public string EMail { get; set; }
+
+        [Key(3)]
+        public string Remark { get; set; }
+    }
+}
---
+++ Samples/Performance/ServerApp/Program.cs
@@ -0,0 +1,100 @@
+﻿using BeetleX.XRPC;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServerApp
+{
+    class Program
+    {
+        private static XRPCServer mXRPCServer;
+
+        static void Main(string[] args)
+        {
+            mXRPCServer = new XRPCServer();
+            mXRPCServer.Register(typeof(Program).Assembly);
+            mXRPCServer.Open();
+            Console.Read();
+        }
+    }
+
+
+    [Controller(typeof(IUserService))]
+    public class UserService : IUserService
+    {
+        public Task<User> Add(string name, string email, string city, string remark)
+        {
+            User user = new User();
+            user.Name = name;
+            user.EMail = email;
+            user.City = city;
+            user.Remark = remark;
+            return Task.FromResult(user);
+        }
+
+        public Task<List<User>> List(int count)
+        {
+            List<User> result = new List<User>();
+            for (int i = 0; i < count; i++)
+            {
+                User user = new User();
+                user.ID = Guid.NewGuid().ToString("N");
+                user.City = "GuangZhou";
+                user.EMail = "Henryfan@msn.com";
+                user.Name = "henryfan";
+                user.Remark = "http://ikende.com";
+                result.Add(user);
+            }
+            return Task.FromResult(result);
+        }
+
+        public bool Login(string name, string pwd)
+        {
+            return (name == "admin" && pwd == "123456");
+        }
+
+        public User Modify(User user)
+        {
+            return user;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("user saved");
+        }
+    }
+
+
+    public interface IUserService
+    {
+        bool Login(string name, string pwd);
+
+        Task<User> Add(string name, string email, string city, string remark);
+
+        void Save();
+
+        User Modify(User user);
+
+        Task<List<User>> List(int count);
+    }
+
+    [MessagePackObject]
+    public class User
+    {
+        [Key(4)]
+        public string ID { get; set; }
+
+        [Key(0)]
+        public string Name { get; set; }
+
+        [Key(1)]
+        public string City { get; set; }
+
+        [Key(2)]
+        public string EMail { get; set; }
+
+        [Key(3)]
+        public string Remark { get; set; }
+    }
+}
